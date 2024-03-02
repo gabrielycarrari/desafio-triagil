239ea33e1c39a8f44a56517418b97dc28ec27752
Gabriely Carrari <gabymcarrari@gmail.com>
2024-03-01 21:18:42 -0300

    ":tada: Commit inicial + Configurações iniciais"

---
+++ desafio-triagil/src/test/java/com/gabriely/desafiotriagil/DesafioTriagilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriely.desafiotriagil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTriagilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
