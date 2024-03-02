b432b1a73618b559f770c84154f9eb78534b018b
Gabriely Carrari <gabymcarrari@gmail.com>
2024-03-02 17:24:20 +0000

    :bricks: ci: Ajustes na configuração do ambiente

---
+++ .docker/db/startdb.sql
@@ -0,0 +1,10 @@
+-- Arquivo de Configuração do Banco de dados
+
+SET client_encoding = 'UTF8';
+
+-- CRIA O DATABASE
+CREATE DATABASE desafiodb;
+
+
+-- Conecta ao banco de dados desafiodb criado anteriormente
+\c desafiodb;
